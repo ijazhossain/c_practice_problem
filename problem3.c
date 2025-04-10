fff0296d82d62862af2acfb2d2be62bcd45a1a0b
ijazhossain <ijaz.hossain35@gmail.com>
2025-04-10 13:20:32 +0600

    fifth prob added

--- problem3.c
+++ problem3.c
@@ -5,7 +5,7 @@ int main(){
     double C;
     char D;
     scanf("%d %lld %lf %c",&A, &B, &C, &D);
-    printf("%d\n%lld\n%lf\n%c\n",A,B,C,D);
+    printf("%d\n%lld\n%.2lf\n%c\n",A,B,C,D);
     return 0;
 }
 
